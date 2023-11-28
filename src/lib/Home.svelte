7b6548e7eb4ca4c6b0263beac41b2fdac173d5df
jj199 <joel_joy@live.com>
2023-11-28 20:04:31 +1100

    test

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,8 +2,8 @@
 	<div
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
-		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		Get your personalised travel <br class="hidden md:block" /> plans with 
+		<br class="hidden md:block" /> with AI
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
@@ -12,12 +12,6 @@
 		>
 			Get recommendations
 		</button>
-		<a
-			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
-			target="_blank"
-			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
-		>
-			Learn how it's built
-		</a>
+
 	</div>
 </div>
